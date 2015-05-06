cfef5945a43ab20469a002937a50539da1219c5b
Lwelch45 <lwelch1@ithaca.edu>
2015-05-06 15:59:24 -0400

    new package - added aws keys

--- src/main/scala/yelp/Main.scala
+++ src/main/scala/yelp/Main.scala
@@ -11,17 +11,20 @@ object Main {
 
   def main(args: Array[String]) {
     val sparkConf = new SparkConf().setAppName("YelpDataSetChallenge")
-      .set("fs.s3n.awsAccessKeyId","AKIAIPEMZJ3VZMJ3MRGA")
-      .set("fs.s3n.awsSecretAccessKey","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")
       .set("spark.executor.uri", "https://s3.amazonaws.com/jimi-yelp/spark-1.3.0-bin-hadoop2.4.tgz")
 
     val sc = new SparkContext(sparkConf)
     val sqlContext = new SQLContext(sc)
 
-    val bf = sqlContext.jsonFile("s3n://AKIAIPEMZJ3VZMJ3MRGA:Y/oLyxBKk/e9/es+7B+rQexqEZNrciVFscnKDCSX@jimi-yelp/yelp_academic_dataset_business.json")
+    val hadoopConf=sc.hadoopConfiguration;
+    hadoopConf.set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+    hadoopConf.set("fs.s3.awsAccessKeyId", "AKIAIPEMZJ3VZMJ3MRGA")
+    hadoopConf.set("fs.s3.awsSecretAccessKey", "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")
+
+    val bf = sqlContext.jsonFile("s3n://jimi-yelp/yelp_academic_dataset_business.json")
     bf.registerTempTable("business")
 
-    val rf = sqlContext.jsonFile("https://s3.amazonaws.com/jimi-yelp/yelp_academic_dataset_review.json")
+    val rf = sqlContext.jsonFile("s3n://jimi-yelp/yelp_academic_dataset_review.json")
     rf.registerTempTable("review")
 
     val reviewExperiment = new Review(sc,sqlContext,"", 5)
